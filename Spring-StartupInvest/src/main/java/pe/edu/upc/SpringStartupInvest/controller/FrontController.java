8e970c37db122961a98dc0b13a8dca8be8f82add
Madness2002 <73316834+Madness2002@users.noreply.github.com>
2021-10-29 21:37:51 -0500

    version-1.0.5-tf

--- Spring-StartupInvest/src/main/java/pe/edu/upc/SpringStartupInvest/controller/FrontController.java
+++ Spring-StartupInvest/src/main/java/pe/edu/upc/SpringStartupInvest/controller/FrontController.java
@@ -10,7 +10,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import ch.qos.logback.core.pattern.Converter;
 import pe.edu.upc.SpringStartupInvest.model.entity.Category;
 import pe.edu.upc.SpringStartupInvest.model.entity.Startup;
 import pe.edu.upc.SpringStartupInvest.service.crud.CategoryService;
