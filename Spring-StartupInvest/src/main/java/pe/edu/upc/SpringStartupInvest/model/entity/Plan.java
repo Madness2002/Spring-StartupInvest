07145281813ba8e71081dce0c4cd4e19f86806e0
Madness2002 <73316834+Madness2002@users.noreply.github.com>
2021-11-03 23:53:52 -0500

    version-1.0.11-tf
    Actualización de la BD (Planes e TypeInvestment)

--- Spring-StartupInvest/src/main/java/pe/edu/upc/SpringStartupInvest/model/entity/Plan.java
+++ Spring-StartupInvest/src/main/java/pe/edu/upc/SpringStartupInvest/model/entity/Plan.java
@@ -22,33 +22,43 @@ import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
 @Entity
-@Table(name = "Plans",indexes = { @Index(columnList = "plan_id", name = "plans_index_plan_id"),
-		                          @Index(columnList = "plan_name", name = "plans_index_plan_name")},
-            uniqueConstraints = { @UniqueConstraint( columnNames = { "plan_name" } ) })
+@Table(name = "Plans", indexes = { @Index(columnList = "plan_id", name = "plans_index_plan_id"),
+		@Index(columnList = "plan_name", name = "plans_index_plan_name") }, uniqueConstraints = {
+				@UniqueConstraint(columnNames = { "plan_name" }) })
 @SequenceGenerator(name = "Plans_monetary_plan_id_seq", initialValue = 100, allocationSize = 1)
 public class Plan {
 	@Id
 	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "Plans_monetary_plan_id_seq")
-	@Column(name = "plan_id", length=5, nullable = false)
+	@Column(name = "plan_id", length = 5, nullable = false)
 	private Integer id;
 
 	@NotBlank(message = "La nombre no debe estar en blanco")
-	@NotNull(message="La nombre debe contener valor")
-	@Size(max=40,message="El tamaño no debe ser mayor a 40")
+	@NotNull(message = "La nombre debe contener valor")
+	@Size(max = 40, message = "El tamaño no debe ser mayor a 40")
 	@Column(name = "plan_name", length = 40, nullable = false)
 	private String name;
 
-	@DecimalMax(value="9999999999.99")
-	@DecimalMin(value="0.99")
+	@DecimalMax(value = "9999999999.99")
+	@DecimalMin(value = "0.99")
 	@Column(name = "plan_amount", columnDefinition = "DECIMAL(12,2)", nullable = false)
 	private Double amount;
+
+	@NotBlank(message = "El descripción no debe estar en blanco")
+	@NotNull(message = "La descripción debe contener valor")
+	@Size(max = 500, message = "El tamaño no debe ser mayor a 500")
+	@Column(name = "plan_description", length = 500, nullable = false)
+	private String description;
+
+	@Column(name = "plan_state", nullable = false)
+	private Boolean state;
 
 	@OneToMany(mappedBy = "plan", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
 	private List<PlanHistory> plansHistory;
 
 	public Plan() {
-		plansHistory=new ArrayList<PlanHistory>();
+		plansHistory = new ArrayList<PlanHistory>();
 	}
+
 	public Plan(Integer id, String name, Double amount, List<PlanHistory> plansHistory) {
 		super();
 		this.id = id;
@@ -88,7 +98,5 @@ public class Plan {
 	public void setPlansHistory(List<PlanHistory> plansHistory) {
 		this.plansHistory = plansHistory;
 	}
-	
-	
-	
+
 }
