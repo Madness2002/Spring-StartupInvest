a465fc8158a3bea0fdda660e34aa56f5336fdd5c
sergio14082001 <sergio16082001@gmail.com>
2021-11-04 01:54:23 -0500

    version-1.0.11-tf
    Dashboard administrator

---
+++ Spring-StartupInvest/src/main/java/pe/edu/upc/SpringStartupInvest/controller/AdministratorController.java
@@ -0,0 +1,34 @@
+package pe.edu.upc.SpringStartupInvest.controller;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import pe.edu.upc.SpringStartupInvest.model.entity.Category;
+import pe.edu.upc.SpringStartupInvest.model.entity.Startup;
+import pe.edu.upc.SpringStartupInvest.service.crud.CategoryService;
+import pe.edu.upc.SpringStartupInvest.service.crud.StartupService;
+import pe.edu.upc.SpringStartupInvest.util.startup.CompareAmounth;
+import pe.edu.upc.SpringStartupInvest.util.startup.DateTimeUtil;
+
+@Controller
+@RequestMapping("/startupinvest")//dominio base 
+public class AdministratorController {
+	@Autowired
+	private CategoryService categoryService;
+
+	@Autowired
+	private StartupService startupService;
+
+	@GetMapping("administrator")// 
+	public String index() {
+		return "dashboard-administrator/dashboard-administrator";
+	}
+	
+}
